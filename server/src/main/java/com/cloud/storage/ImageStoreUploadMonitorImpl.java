534abb7e46a2c139e02232bc71b57f89e2953896
Abhisar Sinha <63767682+abh1sar@users.noreply.github.com>
2026-03-09 10:19:56 +0530

    secondary storage resource limit for upload
    (cherry picked from commit 021a18dd98338de9772b3b7c89cc43802d1a862e)

--- server/src/main/java/com/cloud/storage/ImageStoreUploadMonitorImpl.java
+++ server/src/main/java/com/cloud/storage/ImageStoreUploadMonitorImpl.java
@@ -26,10 +26,10 @@ import javax.inject.Inject;
 import javax.naming.ConfigurationException;
 
 import com.cloud.agent.api.to.OVFInformationTO;
-import com.cloud.exception.ResourceAllocationException;
 import com.cloud.resourcelimit.CheckedReservation;
 import com.cloud.user.Account;
 import com.cloud.user.dao.AccountDao;
+import com.cloud.user.AccountManager;
 import org.apache.cloudstack.engine.subsystem.api.storage.DataStore;
 import org.apache.cloudstack.engine.subsystem.api.storage.DataStoreManager;
 import org.apache.cloudstack.engine.subsystem.api.storage.EndPoint;
@@ -544,22 +544,6 @@ public class ImageStoreUploadMonitorImpl extends ManagerBase implements ImageSto
                                     break;
                                 }
                             }
-
-                            Account owner = accountDao.findById(template.getAccountId());
-                            long templateSize = answer.getVirtualSize();
-
-                            try (CheckedReservation secondaryStorageReservation = new CheckedReservation(owner, Resource.ResourceType.secondary_storage, null, null, templateSize, reservationDao, _resourceLimitMgr)) {
-                                _resourceLimitMgr.incrementResourceCount(owner.getId(), Resource.ResourceType.secondary_storage, templateSize);
-                            } catch (ResourceAllocationException e) {
-                                tmpTemplateDataStore.setDownloadState(VMTemplateStorageResourceAssoc.Status.UPLOAD_ERROR);
-                                tmpTemplateDataStore.setState(State.Failed);
-                                stateMachine.transitTo(tmpTemplate, VirtualMachineTemplate.Event.OperationFailed, null, _templateDao);
-                                msg = String.format("Upload of template [%s] failed because its owner [%s] does not have enough secondary storage space available.", template.getUuid(), owner.getUuid());
-                                logger.warn(msg);
-                                sendAlert = true;
-                                break;
-                            }
-
                             stateMachine.transitTo(tmpTemplate, VirtualMachineTemplate.Event.OperationSucceeded, null, _templateDao);
                             //publish usage event
                             String etype = EventTypes.EVENT_TEMPLATE_CREATE;
