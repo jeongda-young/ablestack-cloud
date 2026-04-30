9a357e127543e44b4b9e6e1e729f86146ccc39b4
Gean Jair Silva <89494158+GeanJS@users.noreply.github.com>
2026-04-30 05:46:26 -0300

    Correction of the user responsible for the  event (#13066)
    Co-authored-by: gean.silva <gean.silva@scclouds.com.br>
    (cherry picked from commit 92d82989e327470c48d3ad2123a93011c0fb5ae3)

--- server/src/main/java/com/cloud/resourcelimit/ResourceLimitManagerImpl.java
+++ server/src/main/java/com/cloud/resourcelimit/ResourceLimitManagerImpl.java
@@ -1031,7 +1031,8 @@ public class ResourceLimitManagerImpl extends ManagerBase implements ResourceLim
 
         ResourceLimitVO limit = _resourceLimitDao.findByOwnerIdAndTypeAndTag(ownerId, ownerType, resourceType, tag);
 
-        ActionEventUtils.onActionEvent(caller.getId(), caller.getAccountId(),
+        Long callingUserId = CallContext.current().getCallingUserId();
+        ActionEventUtils.onActionEvent(callingUserId, caller.getAccountId(),
                 caller.getDomainId(), EventTypes.EVENT_RESOURCE_LIMIT_UPDATE,
                 "Resource limit updated. Resource Type: " + resourceType.toString() + ", New Value: " + max,
                 ownerResourceId, ownerResourceType.toString());
