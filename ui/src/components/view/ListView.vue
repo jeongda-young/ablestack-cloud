64683ca81c19deec9ac40fd2f8797eff66f8d487
Abhishek Kumar <abhishek.mrt22@gmail.com>
2026-05-03 22:09:41 +0530

    ui: fix webhook filters listing (#13068)
    (cherry picked from commit a17bff9ba8770349c3b8344cb45778e89d4379f4)

--- ui/src/components/view/ListView.vue
+++ ui/src/components/view/ListView.vue
@@ -1042,7 +1042,7 @@
             style="margin-left: 5px"
             :actions="actions"
             :resource="record"
-            :enabled="quickViewEnabled() && actions.length > 0"
+            :enabled="quickViewEnabled(actions, columns, column.key)"
             @exec-action="$parent.execAction"
           />
         </template>
