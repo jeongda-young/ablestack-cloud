apache/cloudstack

Add per-domain OAuth (Google, GitHub) provider support (#12702)
* Add domain_id to oauth_provider table and VO

* Add domain-aware methods to OauthProviderDao

* Add domainId parameter to OAuth provider API commands and response

* Add domain support to OAuth2AuthManager

* Add domain-aware OAuth verification

* Add domain support to ListOAuthProvidersCmd and update related tests

* fix domain path issue

* Add domainId support to OAuth provider

* Return domain name and UUID in OAuth provider API responses using ApiDBUtils

* Refactor domain ID resolution in VerifyOAuthCodeAndGetUserCmd to improve code clarity

* Enhance OAuth2 plugin support for domain-level configuration and authentication checks

* Update OAuth2 tests and VerifyOAuthCodeAndGetUserCmdTest

* Add method to find OAuth provider by domain with global fallback

* Update OAuth provider configuration to use 'domain' instead of 'domainid' in columns and details

* Refactor OAuth provider methods to support domain-level queries and enhance user verification

* Add caching for access token retrieval in GithubOAuth2Provider

* Refactor access token checks in GithubOAuth2Provider to use StringUtils for improved readability and consistency

* Refactor null checks to use utility for improved readability and consistency

* Update OAuth2UserAuthenticatorTest to include domainId in user verification method

* Remove unnecessary blank line and unused imports in OAuth provider command classes

* Refactor and cleanup

* Remove unnecessary blank lines

* Enhance RegisterOAuthProviderCmdTest with additional provider mock data

* Remove startup gate from OAuth plugin initialization to support dynamic config toggling

* Add strictScope to ConfigKey to disable global fallback for domain-scoped oauth2.enabled

* Add domain-scoped provider filtering to listOauthProvider and centralize domain resolution in OAuth2AuthManager

* Add External OAuth tab with domain-scoped provider selection to login page

* code cleanup

* test

ui/src/locales/index.js
@@ -18,46 +18,54 @@
 import { createI18n } from 'vue-i18n'
 import { vueProps } from '@/vue-app'
 
+const FALLBACK_LANG = 'en'
 const loadedLanguage = []
 const messages = {}
 
 export const i18n = createI18n({
-  locale: 'en',
-  fallbackLocale: 'en',
+  locale: FALLBACK_LANG,
+  fallbackLocale: FALLBACK_LANG,
   silentTranslationWarn: true,
   messages: messages,
   silentFallbackWarn: true,
   warnHtmlInMessage: 'off'
 })
 
-export function loadLanguageAsync (lang) {
-  if (!lang) {
-    const locale = vueProps.$localStorage.get('LOCALE')
-    lang = (!locale || typeof locale === 'object') ? 'en' : locale
+function applyMessages (lang, message) {
+  if (message && Object.keys(message).length > 0) {
+    i18n.global.setLocaleMessage(lang, message)
+    messages[lang] = message
   }
-  if (loadedLanguage.includes(lang)) {
-    return Promise.resolve(setLanguage(lang))
-  }
-
-  return fetch(`locales/${lang}.json?ts=${Date.now()}`)
-    .then(response => response.json())
-    .then(json => Promise.resolve(setLanguage(lang, json)))
-}
-
-function setLanguage (lang, message) {
-  if (i18n) {
-    i18n.global.locale = lang
-
-    if (message && Object.keys(message).length > 0) {
-      i18n.global.setLocaleMessage(lang, message)
-    }
-  }
-
   if (!loadedLanguage.includes(lang)) {
     loadedLanguage.push(lang)
   }
+}
 
-  if (message && Object.keys(message).length > 0) {
-    messages[lang] = message
+function fetchLocale (lang) {
+  return fetch(`locales/${lang}.json?ts=${Date.now()}`)
+    .then(response => response.json())
+    .then(json => applyMessages(lang, json))
+}
+
+export function loadLanguageAsync (lang) {
+  if (!lang) {
+    const locale = vueProps.$localStorage.get('LOCALE')
+    lang = (!locale || typeof locale === 'object') ? FALLBACK_LANG : locale
   }
+
+  // Always keep the fallback locale's messages loaded so $t() degrades
+  // to readable English instead of raw keys when a translation is missing.
+  const ensureFallback = loadedLanguage.includes(FALLBACK_LANG)
+    ? Promise.resolve()
+    : fetchLocale(FALLBACK_LANG)
+
+  const ensureTarget = (lang === FALLBACK_LANG || loadedLanguage.includes(lang))
+    ? ensureFallback
+    : ensureFallback.then(() => fetchLocale(lang))
+
+  // Activate locale after messages are in place so the first render
+  // already has the translations and avoids a flash of raw keys.
+  return ensureTarget.then(() => {
+    i18n.global.locale = lang
+  })
 }

jeongda-young/ablestack-cloud

(cherry picked from commit 33c3967b6b636ff50a644be8f445c558b5076051)

ui/src/locales/index.js
@@ -18,46 +18,54 @@
 import { createI18n } from 'vue-i18n'
 import { vueProps } from '@/vue-app'
 
+const FALLBACK_LANG = 'ko_KR'
 const loadedLanguage = []
 const messages = {}
 
 export const i18n = createI18n({
-  locale: 'ko_KR',
-  fallbackLocale: 'ko_KR',
+  locale: FALLBACK_LANG,
+  fallbackLocale: FALLBACK_LANG,
   silentTranslationWarn: true,
   messages: messages,
   silentFallbackWarn: true,
   warnHtmlInMessage: 'off'
 })
 
-export function loadLanguageAsync (lang) {
-  if (!lang) {
-    const locale = vueProps.$localStorage.get('LOCALE')
-    lang = (!locale || typeof locale === 'object') ? 'ko_KR' : locale
+function applyMessages (lang, message) {
+  if (message && Object.keys(message).length > 0) {
+    i18n.global.setLocaleMessage(lang, message)
+    messages[lang] = message
   }
-  if (loadedLanguage.includes(lang)) {
-    return Promise.resolve(setLanguage(lang))
-  }
-
-  return fetch(`locales/${lang}.json?ts=${Date.now()}`)
-    .then(response => response.json())
-    .then(json => Promise.resolve(setLanguage(lang, json)))
-}
-
-function setLanguage (lang, message) {
-  if (i18n) {
-    i18n.global.locale = lang
-
-    if (message && Object.keys(message).length > 0) {
-      i18n.global.setLocaleMessage(lang, message)
-    }
-  }
-
   if (!loadedLanguage.includes(lang)) {
     loadedLanguage.push(lang)
   }
+}
 
-  if (message && Object.keys(message).length > 0) {
-    messages[lang] = message
+function fetchLocale (lang) {
+  return fetch(`locales/${lang}.json?ts=${Date.now()}`)
+    .then(response => response.json())
+    .then(json => applyMessages(lang, json))
+}
+
+export function loadLanguageAsync (lang) {
+  if (!lang) {
+    const locale = vueProps.$localStorage.get('LOCALE')
+    lang = (!locale || typeof locale === 'object') ? FALLBACK_LANG : locale
   }
+
+  // Always keep the fallback locale's messages loaded so $t() degrades
+  // to readable Korean instead of raw keys when a translation is missing.
+  const ensureFallback = loadedLanguage.includes(FALLBACK_LANG)
+    ? Promise.resolve()
+    : fetchLocale(FALLBACK_LANG)
+
+  const ensureTarget = (lang === FALLBACK_LANG || loadedLanguage.includes(lang))
+    ? ensureFallback
+    : ensureFallback.then(() => fetchLocale(lang))
+
+  // Activate locale after messages are in place so the first render
+  // already has the translations and avoids a flash of raw keys.
+  return ensureTarget.then(() => {
+    i18n.global.locale = lang
+  })
 }