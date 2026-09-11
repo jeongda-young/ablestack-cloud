apache/cloudstack

Addition of the status filter in the backup listing API  (#13254)
Co-authored-by: gean.silva <gean.silva@scclouds.com.br>
Co-authored-by: Gean Jair Silva <89494158+GeanJS@users.noreply.github.com>
Co-authored-by: Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>

ui/src/config/section/storage.js
@@ -495,7 +495,7 @@ export default {
       columns: ['name', 'status', 'size', 'virtualsize', 'virtualmachinename', 'backupofferingname', 'intervaltype', 'type', 'created', 'account', 'domain', 'zone'],
       details: ['name', 'description', 'virtualmachinename', 'id', 'intervaltype', 'type', 'externalid', 'size', 'virtualsize', 'volumes', 'backupofferingname', 'zone', 'account', 'domain', 'created'],
       searchFilters: () => {
-        var filters = ['name', 'zoneid', 'domainid', 'account', 'backupofferingid']
+        var filters = ['name', 'zoneid', 'domainid', 'account', 'backupofferingid', 'status']
         return filters
       },
       tabs: [

jeongda-young/ablestack-cloud

(cherry picked from commit 2032bbd7f678b4a224d8a7d107902496655f1b1e)

ui/src/config/section/storage.js
@@ -529,7 +529,7 @@ export default {
       columns: ['name', 'status', 'size', 'virtualsize', 'virtualmachinename', 'backupofferingname', 'intervaltype', 'type', 'created', 'account', 'domain', 'zone'],
       details: ['name', 'description', 'virtualmachinename', 'id', 'intervaltype', 'type', 'externalid', 'size', 'virtualsize', 'volumes', 'backupofferingname', 'zone', 'account', 'domain', 'created'],
       searchFilters: () => {
-        var filters = ['name', 'zoneid', 'domainid', 'account', 'backupofferingname']
+        var filters = ['name', 'zoneid', 'domainid', 'account', 'backupofferingname', 'status']
         return filters
       },
       tabs: [