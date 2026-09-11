apache/cloudstack

Linstor: Add controller token auth support (#13470)
* linstor: update java-linstor to 0.7.0 to support auth token api

With Linstor 1.34.0 a new authentication mode is supported:
* Bearer token

To support that it had to be implemented in the java-linstor library
and we need to store the auth token per storage pool.
Also per default with this auth mode Linstor will run with
HTTPS enabled, so we also have to support that.

* ui: seed default-on toggles in zone wizard while hidden

StaticInputsForm.fillValue() only seeded defaults for currently-displayed
fields, so a display-gated switch with checked:true bound to an undefined
value and rendered as off once revealed. Seed checked switch/checkbox
fields even while hidden, so the Linstor 'Allow self-signed certificate'
toggle defaults on in the zone creation wizard.

ui/src/views/infra/AddPrimaryStorage.vue
@@ -406,6 +406,18 @@
             </template>
             <a-input v-model:value="form.resourcegroup" :placeholder="$t('message.linstor.resourcegroup.description')" />
           </a-form-item>
+          <a-form-item name="linstorApiToken" ref="linstorApiToken">
+            <template #label>
+              <tooltip-label :title="$t('label.linstor.apitoken')" :tooltip="$t('message.linstor.apitoken.description')"/>
+            </template>
+            <a-input v-model:value="form.linstorApiToken" :placeholder="$t('message.linstor.apitoken.description')" />
+          </a-form-item>
+          <a-form-item name="linstorInsecureSsl" ref="linstorInsecureSsl">
+            <template #label>
+              <tooltip-label :title="$t('label.linstor.ssl.insecure')" :tooltip="$t('message.linstor.ssl.insecure.description')"/>
+            </template>
+            <a-switch v-model:checked="form.linstorInsecureSsl" />
+          </a-form-item>
         </div>
         <a-form-item name="selectedTags" ref="selectedTags">
           <template #label>
@@ -480,7 +492,8 @@ export default {
       this.form = reactive({
         scope: 'cluster',
         hypervisor: this.hypervisors[0],
-        provider: 'DefaultPrimary'
+        provider: 'DefaultPrimary',
+        linstorInsecureSsl: true
       })
       this.rules = reactive({
         zone: [{ required: true, message: this.$t('label.required') }],
@@ -896,6 +909,10 @@ export default {
           url = this.linstorURL(server)
           values.managed = false
           params['details[0].resourceGroup'] = values.resourcegroup
+          if (values.linstorApiToken && values.linstorApiToken.length > 0) {
+            params['details[0].lin.auth.apitoken'] = values.linstorApiToken
+          }
+          params['details[0].lin.ssl.insecure'] = values.linstorInsecureSsl ? 'true' : 'false'
           if (values.capacityIops && values.capacityIops.length > 0) {
             params.capacityIops = values.capacityIops.split(',').join('')
           }

jeongda-young/ablestack-cloud

(cherry picked from commit 0231e67b9d9adf1ac0b4a4bd7a9acbf654e09cbc)

ui/src/views/infra/AddPrimaryStorage.vue
@@ -476,6 +476,18 @@
             </template>
             <a-input v-model:value="form.resourcegroup" :placeholder="$t('message.linstor.resourcegroup.description')" />
           </a-form-item>
+          <a-form-item name="linstorApiToken" ref="linstorApiToken">
+            <template #label>
+              <tooltip-label :title="$t('label.linstor.apitoken')" :tooltip="$t('message.linstor.apitoken.description')"/>
+            </template>
+            <a-input-password v-model:value="form.linstorApiToken" :placeholder="$t('message.linstor.apitoken.description')" />
+          </a-form-item>
+          <a-form-item name="linstorInsecureSsl" ref="linstorInsecureSsl">
+            <template #label>
+              <tooltip-label :title="$t('label.linstor.ssl.insecure')" :tooltip="$t('message.linstor.ssl.insecure.description')"/>
+            </template>
+            <a-switch v-model:checked="form.linstorInsecureSsl" />
+          </a-form-item>
         </div>
         <a-form-item name="selectedTags" ref="selectedTags">
           <template #label>
@@ -550,7 +562,8 @@ export default {
       this.form = reactive({
         scope: 'cluster',
         hypervisor: this.hypervisors[0],
-        provider: 'ABLESTACK'
+        provider: 'ABLESTACK',
+        linstorInsecureSsl: false
       })
       this.rules = reactive({
         zone: [{ required: true, message: this.$t('label.required') }],
@@ -1047,6 +1060,10 @@ export default {
           url = this.linstorURL(server)
           values.managed = false
           params['details[0].resourceGroup'] = values.resourcegroup
+          if (values.linstorApiToken && values.linstorApiToken.length > 0) {
+            params['details[0].lin.auth.apitoken'] = values.linstorApiToken
+          }
+          params['details[0].lin.ssl.insecure'] = values.linstorInsecureSsl ? 'true' : 'false'
           if (values.capacityIops && values.capacityIops.length > 0) {
             params.capacityIops = values.capacityIops.split(',').join('')
           }