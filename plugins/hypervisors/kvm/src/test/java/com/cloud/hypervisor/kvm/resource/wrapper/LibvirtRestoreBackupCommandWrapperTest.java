5e471dddaa6c4efc6e7ccbe75b9732f75be2ffa8
DH Park <44049806+dhslove@users.noreply.github.com>
2026-09-11 02:28:27 +0000

    Isolate NAS restore unit tests from native QEMU inspection

--- plugins/hypervisors/kvm/src/test/java/com/cloud/hypervisor/kvm/resource/wrapper/LibvirtRestoreBackupCommandWrapperTest.java
+++ plugins/hypervisors/kvm/src/test/java/com/cloud/hypervisor/kvm/resource/wrapper/LibvirtRestoreBackupCommandWrapperTest.java
@@ -28,15 +28,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Map;
 
 import org.apache.cloudstack.backup.BackupAnswer;
 import org.apache.cloudstack.backup.RestoreBackupCommand;
 import org.apache.cloudstack.storage.to.PrimaryDataStoreTO;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockedStatic;
+import org.mockito.MockedConstruction;
+import org.apache.cloudstack.utils.qemu.QemuImg;
+import org.apache.cloudstack.utils.qemu.QemuImgFile;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -53,12 +58,20 @@ public class LibvirtRestoreBackupCommandWrapperTest {
     private LibvirtRestoreBackupCommandWrapper wrapper;
     private LibvirtComputingResource libvirtComputingResource;
     private RestoreBackupCommand command;
+    private MockedConstruction<QemuImg> qemuImages;
 
     @Before
     public void setUp() {
+        qemuImages = Mockito.mockConstruction(QemuImg.class, (mock, context) ->
+                when(mock.info(any(QemuImgFile.class))).thenReturn(Map.of()));
         wrapper = new LibvirtRestoreBackupCommandWrapper();
         libvirtComputingResource = Mockito.mock(LibvirtComputingResource.class);
         command = Mockito.mock(RestoreBackupCommand.class);
+    }
+
+    @After
+    public void tearDown() {
+        qemuImages.close();
     }
 
     @Test
