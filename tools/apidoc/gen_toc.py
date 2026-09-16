d596e5ef3f3b0bd5000cf4c9a2919e39df0b75f4
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2026-09-16 09:47:16 +0900

    빌드 오류 수정

--- tools/apidoc/gen_toc.py
+++ tools/apidoc/gen_toc.py
@@ -51,6 +51,7 @@ known_categories = {
     'VirtualMachine': 'Virtual Machine',
     'VM': 'Virtual Machine',
     'VmDevice': 'Virtual Machine',
+    'updateVmCloneFlattenBandwidth': 'Virtual Machine',
     'Vnf': 'Virtual Network Functions',
     'VnfTemplate': 'Virtual Network Functions',
     'GuestSubnet': 'Routing',
